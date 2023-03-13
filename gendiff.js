ea4511e9415fdd16a16c78233688ce891ce74939
a.y.kuznetsov <a.y.kuznetsov@fun-box.ru>
2023-03-13 17:51:16 +0300

    Add format argument

--- gendiff.js
+++ gendiff.js
@@ -8,4 +8,8 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.');
 
+program
+  .option('-f, --format <type>', 'output format')
+  .argument('<type>', 'output format');
+
 program.parse();
